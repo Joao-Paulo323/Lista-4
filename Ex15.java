edd195fad80dca9eca53a78a92f9ef67f28ff23e
João Paulo <joaopaulodelanapicoli2@gmail.com>
2026-04-15 11:04:26 -0300

    Update Ex15.java

--- Ex15.java
+++ Ex15.java
@@ -1,4 +1,4 @@
-package Lista4_revisão;
+package  Lista4_Revisão;
 
 import java.util.Scanner;
 
@@ -8,7 +8,10 @@ public class Ex15 {
         System.out.println("Digite o dia e mês de seu nascimento e informaremos se seu signo é Áries");
         int a = scan.nextInt();
         int b = scan.nextInt();
-        if (a>=21 && a<=31 && b ==3 || a<= 19 && a >=1 && b == 4){
+        if (a > 31 || a < 0 || b > 12 || b < 0){
+            System.out.println("Digite um dia ou um mes valido");
+        }
+        else if (a>=21 && a<=31 && b ==3 || a<= 19 && a >=1 && b == 4){
             System.out.println("Seu signo é Áries");
         }
         else {
